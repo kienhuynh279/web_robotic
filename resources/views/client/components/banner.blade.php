f5e5a4bd4c988ba9b91bc654d43687671ffaaf4d
duongductrong <duongductrong06@gmail.com>
2020-11-03 21:32:57 +0700

    add controller

--- resources/views/client/components/banner.blade.php
+++ resources/views/client/components/banner.blade.php
@@ -1,3 +1,24 @@
 <div class="container px-0">
-    <img class="w-100" height="500" src="{{asset("assets/client/img/595x240/02.jpg")}}"  alt="The image">
+
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="w-100" height="500" src="{{asset("assets/client/img/595x240/02.jpg")}}" alt="The image">
+            </div>
+            <div class="carousel-item">
+                <img class="w-100" height="500" src="{{asset("assets/client/img/595x240/02.jpg")}}" alt="The image">
+            </div>
+            <div class="carousel-item">
+                <img class="w-100" height="500" src="{{asset("assets/client/img/595x240/02.jpg")}}" alt="The image">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 </div>
