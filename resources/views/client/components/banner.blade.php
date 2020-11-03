980fbcd991f6ff7069d967b0057b2f7ec573bf89
duongductrong <duongductrong06@gmail.com>
2020-11-03 21:03:30 +0700

    add height for banner

--- resources/views/client/components/banner.blade.php
+++ resources/views/client/components/banner.blade.php
@@ -1,3 +1,3 @@
 <div class="container px-0">
-    <img class="w-100" src="{{asset("assets/client/img/595x240/02.jpg")}}"  alt="The image">
+    <img class="w-100" height="500" src="{{asset("assets/client/img/595x240/02.jpg")}}"  alt="The image">
 </div>
