46ee37b5fce474400ec4f4beb8038453de5d94ad
kienhuynh279 <kienhuynh279@gmail.com>
2020-11-15 00:24:26 +0700

    Fix

--- app/Http/Controllers/client/ServiceController.php
+++ app/Http/Controllers/client/ServiceController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Service;
+use App\Models\Type;
 
 class ServiceController extends Controller
 {
     public function index()
     {
-        return view('client.pages.service.index');
+        $service = Service::where('status',1)->get();
+        $type = Type::where('status', 1)->get();
+        return view('client.pages.service.index',[
+            'services' => $service,
+            'types' =>$type
+        ]);
     }
 }
--- resources/views/client/pages/service/index.blade.php
+++ resources/views/client/pages/service/index.blade.php
@@ -9,93 +9,24 @@
 	   <div class="container">
             <div class="center wow fadeInDown">
                 <h2>SOLUTIONS</h2>
-               
             </div>
-
             <div class="row">
-
-                <div class="col-sm-6 col-md-4">
-                    <div class="media services-wrap-detail wow fadeInDown">
-                        <div class="pull-left">
-                            <img class="img-responsive" src="{{ asset('assets/client/images/services/s1.png') }}">
+                @foreach($types as $type)
+                    @foreach($services as $service)
+                        @if($service->TypeId === 4)
+                        <div class="col-sm-6 col-md-4">
+                            <div class="media services-wrap-detail wow fadeInDown">
+                                <div class="pull-left">
+                                    <img class="img-responsive" src="{{ asset('assets/client/images/services/s1.png') }}">
+                                </div>
+                                <div class="media-body">
+                                    <p>{{ $service->Title }}</p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="media-body">
-                            
-                            <p>2D, 3D laser, Visual SLAM (simultaneous localization and mapping) and navigation for automated mobile robot</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-sm-6 col-md-4">
-                    <div class="media services-wrap-detail wow fadeInDown">
-                        <div class="pull-left">
-                            <img class="img-responsive" src="{{ asset('assets/client/images/services/s2.png') }}">
-                        </div>
-                        <div class="media-body">
-                           
-                            <p>AI and computer vision  application for cleaning and logistic robot</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-sm-6 col-md-4">
-                    <div class="media services-wrap-detail wow fadeInDown">
-                        <div class="pull-left">
-                            <img class="img-responsive" src="{{ asset('assets/client/images/services/s3.png') }}">
-                        </div>
-                        <div class="media-body">
-                        
-                            <p>Design of  multiple robot system</p>
-                        </div>
-                    </div>
-                </div>  
-
-                <div class="col-sm-6 col-md-4">
-                    <div class="media services-wrap-detail wow fadeInDown">
-                        <div class="pull-left">
-                            <img class="img-responsive" src="{{ asset('assets/client/images/services/s4.png') }}">
-                        </div>
-                        <div class="media-body">
-                            
-                            <p>IOT based Robot Operation System (ROS)</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-sm-6 col-md-4">
-                    <div class="media services-wrap-detail wow fadeInDown">
-                        <div class="pull-left">
-                            <img class="img-responsive" src="{{ asset('assets/client/images/services/s5.png') }}">
-                        </div>
-                        <div class="media-body">
-                            
-                            <p>2D and 3D laser based object detection applied in docking and charging and obstacle avoidance, SLAM  of robot</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-sm-6 col-md-4">
-                    <div class="media services-wrap-detail wow fadeInDown">
-                        <div class="pull-left">
-                            <img class="img-responsive" src="{{ asset('assets/client/images/services/s6.png') }}">
-                        </div>
-                        <div class="media-body">
-                            
-                            <p>Adaptive generating morphology and path planning strategies while deployment in new environments</p>
-                        </div>
-                    </div>
-                </div>
-				<div class="col-sm-6 col-md-4">
-                    <div class="media services-wrap-detail wow fadeInDown">
-                        <div class="pull-left">
-                            <img class="img-responsive" src="{{ asset('assets/client/images/services/s7.png') }}">
-                        </div>
-                        <div class="media-body">
-                            
-                            <p>Adaptive control of  robot network</p>
-                        </div>
-                    </div>
-                </div>  				
+                        @endif
+                    @endforeach
+                @endforeach	
             </div><!--/.row-->
         </div><!--/.container-->
     </section><!--/#services-->
