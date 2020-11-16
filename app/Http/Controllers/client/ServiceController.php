254c214fb933fb24c7db2e9bff51945558d06f31
duongductrong <duongductrong06@gmail.com>
2020-11-16 23:12:24 +0700

    fix logic render items

--- app/Http/Controllers/client/ServiceController.php
+++ app/Http/Controllers/client/ServiceController.php
@@ -23,8 +23,12 @@ class ServiceController extends Controller
     public function solution()
     {
         $solution = Service::where('status', 1)->get();
-        $type = Type::where('status', 1)->get();
+        $type = Type::where([
+            ['status', 1],
+            ['Title', 'Solution']
+        ])->first();
         $route = Route::currentRouteName();
+
         return view('client.pages.service.solution',[
             'solutions' => $solution,
             'type' => $type,
@@ -34,11 +38,14 @@ class ServiceController extends Controller
 
     public function traning()
     {   
-        $type = Type::where('status', 1)->get();
+        $type = Type::where([
+            ['status', 1],
+            ['Title', 'Training']
+        ])->first();
         $route = Route::currentRouteName();
-        $traning = Service::where('status', 1)->get();
+        $training = Service::where('status', 1)->get();
         return view('client.pages.service.traning',[
-            'tranings' => $traning, 
+            'trainings' => $training, 
             'type' => $type,
             'route' => $route
         ]);
--- resources/views/client/pages/service/solution.blade.php
+++ resources/views/client/pages/service/solution.blade.php
@@ -1,41 +1,43 @@
 @extends('layouts.client')
 
 @section('title')
-    Solution
+Solution
 @endsection
 
 @section('content')
 
 <section id="services-detail" class="service-item">
     <div class="container">
-         <div class="center wow fadeInDown">
-             <h2>SOLUTIONS</h2>
-            
-         </div>
-        
-         <div class="row">
-            @foreach($type as $i)
-            @if(strtolower($i->Title) === $route)
+        <div class="center wow fadeInDown">
+            <h2>SOLUTIONS</h2>
+
+        </div>
+
+        <div class="row">
             {{-- @dd(strtolower($i->Title)) --}}
             @foreach($solutions as $item)
-                <div class="col-sm-6 col-md-4">
-                    <div class="media services-wrap-detail wow fadeInDown">
-                        <div class="pull-left">
+            @if($type->TypeId === $item->TypeId)
+
+            <div class="col-sm-6 col-md-4">
+                <div class="media services-wrap-detail wow fadeInDown">
+                    <div class="pull-left">
                         <img class="img-responsive" src="{{$item->Image}}">
-                        </div>
-                        <div class="media-body">
+                    </div>
+                    <div class="media-body">
                         <p>{{ $item->Title }}</p>
-                        </div>
                     </div>
                 </div>
-                @break
-            @endforeach
+            </div>
+            @break
             @endif
-            
+
             @endforeach
-         </div><!--/.row-->
-     </div><!--/.container-->
- </section><!--/#services-->
+        </div>
+        <!--/.row-->
+    </div>
+    <!--/.container-->
+</section>
+<!--/#services-->
 
 @endsection
 
