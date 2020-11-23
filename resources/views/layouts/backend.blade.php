b72ec4d807f6b30ac6ef50b1b38b3348a38a8b60
duongductrong <duongductrong06@gmail.com>
2020-11-24 00:31:23 +0700

    fix layout change copyright

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -1016,10 +1016,10 @@
                 <div class="row font-size-sm">
                     <div class="col-sm-6 order-sm-2 mb-1 mb-sm-0 text-center text-sm-right">
                         Crafted with <i class="fa fa-heart text-danger"></i> by <a class="font-w600"
-                            href="https://1.envato.market/ydb" target="_blank">pixelcave</a>
+                            href="{{request()->getSchemeAndHttpHost()}}" target="_blank">{{request()->getHttpHost()}}</a>
                     </div>
                     <div class="col-sm-6 order-sm-1 text-center text-sm-left">
-                        <a class="font-w600" href="https://1.envato.market/r6y" target="_blank">Dashmix</a> &copy; <span
+                        <a class="font-w600" href="{{request()->getSchemeAndHttpHost()}}" target="_blank">{{request()->getHttpHost()}}</a> &copy; <span
                             data-toggle="year-copy"></span>
                     </div>
                 </div>
