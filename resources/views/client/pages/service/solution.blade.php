6079957fd887ecfeac7fbcfe249a78e37e48bf45
duongductrong <duongductrong06@gmail.com>
2020-11-26 23:18:23 +0700

    fix css solutions page item

--- resources/views/client/pages/service/solution.blade.php
+++ resources/views/client/pages/service/solution.blade.php
@@ -13,18 +13,20 @@ Solution
         </div>
         <div class="row">
             @foreach ($solutions as $item)
-
-                <div class="col-sm-6 col-md-4">
-                    <div class="media services-wrap-detail wow fadeInDown">
-                        <div class="">
-                            <img class="img-responsive" style="object-fit: cover; object-position: center; height: 200px; width: 100%" src="{{$item->Image}}">
-                        </div>
-                        <div class="media-body">
+            <div class="col-sm-6 col-md-4">
+                <div class="media services-wrap-detail wow fadeInDown" style="height: auto; max-height: 100%; padding: 15px">
+                    <div class="" style="display: flex">
+                        <img class="img-responsive"
+                            style="object-fit: cover; object-position: center; height: 100px; width: 100px"
+                            src="{{$item->Image}}">
+                        <div class="media-body" style="margin-left: 20px">
                             <p>{{ $item->Title }}</p>
                         </div>
                     </div>
+
                 </div>
-                    
+            </div>
+
             @endforeach
         </div>
         <!--/.row-->
